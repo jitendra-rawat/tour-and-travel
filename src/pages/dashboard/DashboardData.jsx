0cb7785d690c02038cd0bb5822de1cedc73b96ba
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2024-03-15 17:50:57 +0530

    updated

--- src/pages/dashboard/DashboardData.jsx
+++ src/pages/dashboard/DashboardData.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { MdCardTravel } from "react-icons/md";
+import { FaMessage, FaUser } from "react-icons/fa6";
+import { TbBrandBooking } from "react-icons/tb";
+import { LiaBlogSolid } from "react-icons/lia";
+import { CgWebsite } from "react-icons/cg";
+
+const DashboardCard = ({ icon, title, value }) => {
+  const IconComponent = icon; 
+
+  return (
+    <div className='bg-white w-[300px] h-[250px] py-20 px-4 rounded-xl m-4'>
+      {icon && <IconComponent size={50}  className='mx-auto' />} 
+      <p className='text-lg text-gray-700 text-center font-poppins font-bold'>{title}</p>
+      <p className='text-4xl text-gray-700 text-center font-poppins font-semibold'>{value}</p>
+    </div>
+  );
+};
 
 const DashboardData = () => {
-  return (
-    <div>DashboardData</div>
-  )
-}
+  
+  const cardsData = [
+    { icon: MdCardTravel, title: 'Total Tour Packages Added', value: '8' },
+    { icon: FaMessage,title: 'Total Messages Received', value: '18' },
+    { icon: FaUser,title: 'Registered Users', value: '28' },
+    { icon: TbBrandBooking,title: 'Total Booking', value: '48' },
+    { icon: LiaBlogSolid,title: 'Number of Blogs', value: '10' },
+    { icon: CgWebsite,title: 'Website Visits', value: '100' },
+  ];
 
-export default DashboardData
+  return (
+    <section className='flex flex-wrap justify-between'>
+      {cardsData.map((card, index) => (
+        <DashboardCard key={index} {...card} />
+      ))}
+    </section>
+  );
+};
+
+export default DashboardData;
--- src/pages/dashboard/TourPackages.jsx
+++ src/pages/dashboard/TourPackages.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+
+const TourPackageCard = ({ title, location, duration, price }) => {
+  return (
+    <div className="bg-white w-[300px] h-[250px] rounded-lg shadow-md  mx-4 my-6">
+ 
+      <div className="p-6">
+        <h2 className="text-xl font-bold mb-2">{title}</h2>
+        <p className="text-gray-700 mb-2">Location: {location}</p>
+        <p className="text-gray-700 mb-2">Duration: {duration}</p>
+        <p className="text-gray-700 mb-4">Price: {price}</p>
+        <div className="flex justify-between">
+          <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+            Update
+          </button>
+          <button className="bg-black  text-white font-bold py-2 px-4 rounded">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const TourPackages = () => {
-  return (
-    <div>TourPackages</div>
-  )
-}
+ 
+  const tourPackages = [
+    {
+      title: 'Mountain Trek',
+      location: 'Himalayas, Nepal',
+      duration: '5 days',
+      price: '$1000',
+    },
+    {
+      title: 'Desert Safari',
+      location: 'Sahara Desert, Morocco',
+      duration: '3 days',
+      price: '$800',
+    },
+    {
+      title: 'Island Hopping',
+      location: 'Philippines',
+      duration: '7 days',
+      price: '$1200',
+    },
+    {
+      title: 'City Explorer',
+      location: 'New York, USA',
+      duration: '4 days',
+      price: '$900',
+    },
+  ];
 
-export default TourPackages
+  return (
+    <div className="flex flex-wrap justify-between">
+      {tourPackages.map((tour, index) => (
+        <TourPackageCard key={index} {...tour} />
+      ))}
+    </div>
+  );
+};
+
+export default TourPackages;
